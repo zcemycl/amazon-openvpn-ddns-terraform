6ce134a91d7bd204c4f56cae0830f1229b3ea841
zcemycl <lyc010197@gmail.com>
2024-07-11 11:07:47 +0100

    fix linting

--- vars.tf
+++ vars.tf
@@ -1,44 +1,49 @@
 variable "vpc_id" {
-    type = string
+  type = string
 }
 
 variable "prefix" {
-    type = string
+  type = string
 }
 
 variable "openvpn_server_ami" {
-    type = string
-    description = "any ubuntu ami, it does not need to be specific openvpn server ami"
+  type        = string
+  description = "any ubuntu ami, it does not need to be specific openvpn server ami"
 }
 
 variable "subnet_id" {
-    type = string
+  type = string
 }
 
 variable "sg_ids" {
-    type = list(string)
+  type = list(string)
 }
 
 variable "instance_type" {
-    type = string
-    default = "t2.small"
+  type    = string
+  default = "t2.small"
 }
 
 variable "admin_pwd" {
-    type = string
-    sensitive = true
+  type      = string
+  sensitive = true
 }
 
 variable "email" {
-    type = string
-    description = "Email to register Domain name for ssl certificate"
+  type        = string
+  description = "Email to register Domain name for ssl certificate"
 }
 
 variable "subdomain" {
-    type = string
-    description = "xxx.yyy where xxx is subdomain and yyy is domain name, please type xxx value here. "
+  type        = string
+  description = "xxx.yyy where xxx is subdomain and yyy is domain name, please type xxx value here. "
 }
 
 variable "domain" {
-    type = string
+  type = string
+}
+
+variable "AWS_REGION" {
+  type    = string
+  default = "eu-west-2"
 }
